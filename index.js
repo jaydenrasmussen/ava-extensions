25a30dbf136f9f63fa13e8bec36bb79784e276de
Jayden Rasmussen <jayden.rasmussen@me.com>
2018-04-26 22:06:38 -0600

    eslint compatability, git hooks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-'use strict';
+
 // add spies and stubs;
 // is's
-let isArray = (t, module, prop) => t.is(Object.prototype.toString.call(module[prop]), `[object Array]`);
+let isArray = (t, module, prop) => t.is(Object.prototype.toString.call(module[prop]), '[object Array]');
 let isAsync = (t, module, prop) => {
     const Async = (async () => {}).constructor;
     t.true(module[prop] instanceof Async);
@@ -9,14 +9,14 @@ let isAsync = (t, module, prop) => {
 let isClass = (t, module, prop, expected) => t.is(Object.prototype.toString.call(module[prop]), `[object ${expected}]`);
 let isFunction = (t, module, prop) => t.is(typeof module[prop], 'function');
 let isGenerator = (t, module, prop) => {
-    const Generator = (function*(){}).constructor;
+    const Generator = function*(){}.constructor;
     t.is(module[prop] instanceof Generator);
 };
 let isNumber = (t, module, prop) => t.is(typeof module[prop], 'number');
 let isObject = (t, module, prop) => t.is(typeof module[prop], 'object');
 let isString = (t, module, prop) => t.is(typeof module[prop], 'string');
 // nots
-let isNotArray = (t, module, prop) => t.not(Object.prototype.toString.call(module[prop]), `[object Array]`);
+let isNotArray = (t, module, prop) => t.not(Object.prototype.toString.call(module[prop]), '[object Array]');
 let isNotAsync = (t, module, prop) => {
     const Async = (async () => {}).constructor;
     t.false(module[prop] instanceof Async);
@@ -24,7 +24,7 @@ let isNotAsync = (t, module, prop) => {
 let isNotClass = (t, module, prop, expected) => t.not(Object.prototype.toString.call(module[prop]), `[object ${expected}]`);
 let isNotFunction = (t, module, prop) => t.not(typeof module[prop], 'function');
 let isNotGenerator = (t, module, prop) => {
-    const Generator = (function*(){}).constructor;
+    const Generator = function*(){}.constructor;
     t.false(module[prop] instanceof Generator);
 };
 let isNotNumber = (t, module, prop) => t.not(typeof module[prop], 'number');
@@ -50,4 +50,4 @@ module.exports = {
     isString,
     isNotString,
     stub
-}
+};
